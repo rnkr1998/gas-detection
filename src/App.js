e74c62dc1d397478700fbc5312fb77ef3dcb89a5
naveen <naveenreddy551@gmail.com>
2019-11-12 02:22:15 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -7,79 +7,132 @@ import './style.css';
 class App extends Component
 {
 
-componentDidMount()
-{
-  var x=new XMLHttpRequest();
-   x.open("GET","https://api.thingspeak.com/channels/906153/fields/1.json?",true);
-
-x.send();
-x.onload=function()
-{
-     var i;
-    var data=JSON.parse(x.responseText); 
-    
-    var b=data.feeds; 
-    for(i in b)
-    {
-        var c=b[i].field1/3000;
-        var d=c*100;
-        document.getElementById("text").value=b[i].field1+"kg";
+  state = {
+    smoke: "",
+    smokeper:"",
+    smokedef:"",
    
-        document.getElementById("progress").style.width=d+"px";
-        document.getElementById("progress").innerHTML=d+"%";
-    }
-     
-          
-}
-
- this.handler();
-}
+    gasdef:"",
+    gasper:""
+  };
 
 
-handler = () => 
-{
-  var x=new XMLHttpRequest();
-  x.open("GET","https://api.thingspeak.com/channels/228181/fields/2.json?",true);
 
-x.send();
-x.onload=function()
-{
-  var j;
-    var dataa=JSON.parse(x.responseText); 
-     
-    var y=dataa.feeds; 
-    for(j in y)
-    {
-        var f=y[j].field2;
-     
-        document.getElementById("textt").value=y[j].field2;
-       
-        document.getElementById("progresbar").style.width=f+"px";
-        document.getElementById("progresbar").innerHTML=f+"%";
-    } 
+
+
+  componentDidMount() {
+    this.getsmoke();
+    this.interval = setInterval(() => {
+      this.getsmoke();
+    }, 100);
+
+    
+  }
+
+
+
+  getsmoke() {
+    fetch("https://api.thingspeak.com/channels/228181/feeds.json?")
+      .then(res => {
+        return res.json();
+      })
+      .then(res => {
+
+        var b=res.feeds;
+        for(let i in b)
+        {
+          var c=b[i].field3; 
+        
+        if(c<=15)
+        {
+          document.getElementById("progress").style.backgroundColor="red";
+        }
+        else if(c>=16 && c<=70)
+        {
+          document.getElementById("progress").style.backgroundColor="yellow";
+        }
+        else
+        {
+          document.getElementById("progress").style.backgroundColor="#4CAF50";
+        }
+
+
+
+           this.setState({
+            smoke: b[i].field1+"kg"
+          });
+          this.setState({
+            smokedef: b[i].field2
+          });
+          this.setState({
+            smokeper: b[i].field3+"%"
+          });
+          this.setState({
+            gasdef: b[i].field4
+          });
+          this.setState({
+            gasper: b[i].field5+"%"
+          });
+        }
+      
+      });
+  }
+
+
+
+  
+
+
+
+  
+
+
+
+componentWillUnmount() {
+  clearInterval(this.interval);
 }
-}
+
   render()
   {
      return(
+
        <div className="App">
-       <h1 onClick={this.handler}>Weight Percentage</h1>
+         <div className="containerr">
+       <h1>Volume of gas</h1>
 
-       <input type="text" id="text" disabled/>
-<br/><br/>
-<div className="container">
-  <div className="skills" id="progress"></div>
+       <input type="text" id="text" value={this.state.smoke} disabled/>
+       <input type="text" id="smokedef" value={this.state.smokedef} disabled/>
+  
+<div className="container" >
+
+  <div className="skills" id="progress" style={{width:this.state.smokeper}}>{this.state.smokeper}</div>
 </div>
 
-<h1>Smoke Percentage</h1>
-<input type="text" id="textt" disabled />
-<br/><br/>
-<div className="container">
-  <div className="skills" id="progresbar"></div>
 </div>
-</div>  
+
+
+
+
+
+<div className="containerr">
+
+<h1>Environment</h1>
+
+<input type="text" id="textt" value={this.state.smokedef} disabled/>
+
+<p>{this.state.gasdef}</p>                                   
+</div>
+
+</div>
   );
      }
 }
 
 export default App;
+
+
+
+
+
+
+
